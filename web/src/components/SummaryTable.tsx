e0eaa12e6c21601d10979096b082fbc8c1e58920
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2023-01-22 17:19:46 -0300

    SummaryTable component fix

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,7 +13,7 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning();
 
-const minimumSummaryDatesSize = 10 * 7;
+const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
@@ -33,19 +33,21 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {summaryDates.map(date => {
-                    return <HabitDay key={date.toString()} />
+                {summaryDates.map((date) => {
+                    return (
+                        <HabitDay key={date.toString()} />
+                    )
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                        />
+                    );
                 })}
             </div>
-
-            {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-                return (
-                    <div
-                        key={i}
-                        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-                    />
-                );
-            })}
         </div>
     );
 }
